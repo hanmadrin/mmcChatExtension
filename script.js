6338c1ffdc64ce0b68e97339c59e78777dc6608f
hanmadrin <hanmadrin@gmail.com>
2022-10-10 20:49:45 +0600

    save

--- script.js
+++ script.js
@@ -1548,13 +1548,13 @@ const contentScripts = {
             if(hasRepliesToSend.status){
                 contentScripts.showDataOnConsole('has replies to send');
                 sendUnsentMessage = await contentScripts.getUnsentMessagePostIds();
-                console.log('unsent message post ids',sendUnsentMessage);
-                console.log(sendUnsentMessage);
-                console.log(`item_id: ${hasRepliesToSend.item_id}`);
-                const fb_post_id = await contentScripts.postIdByItemId(hasRepliesToSend.item_id);
-                console.log(`fb_post_id: ${fb_post_id}`);
-                sendUnsentMessage =[fb_post_id];
-                console.log(`postIds to send: ${sendUnsentMessage}`);
+                // console.log('unsent message post ids',sendUnsentMessage);
+                // console.log(sendUnsentMessage);
+                // console.log(`item_id: ${hasRepliesToSend.item_id}`);
+                // const fb_post_id = await contentScripts.postIdByItemId(hasRepliesToSend.item_id);
+                // console.log(`fb_post_id: ${fb_post_id}`);
+                // sendUnsentMessage =[fb_post_id];
+                // console.log(`postIds to send: ${sendUnsentMessage}`);
                 await sendUnsentMessageDB.SET(sendUnsentMessage);
             }else{
                 contentScripts.showDataOnConsole('no replies but checking for second message');
